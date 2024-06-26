1dc49eccfb896b67a7fd1d216c01e09287612c64
Nazar <nr14champion@gmail.com>
2024-06-26 21:14:51 +0300

    test commit

---
+++ Assets/Scripts/testScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    public int number = 0;
+    void Start()
+    {
+        number += 100;
+    }
+
+}
