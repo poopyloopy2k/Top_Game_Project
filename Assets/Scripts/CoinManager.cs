016e5592dd725e3fe074fc465e557570f4b7ac90
Likholap Fiodor <114578334+poopyloopy2k@users.noreply.github.com>
2024-06-29 16:41:55 +0300

    add coin bar

--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CoinManager : MonoBehaviour
 {
     public int coinCount;
+    public TextMeshProUGUI coinText;
     // Start is called before the first frame update
     void Start()
     {
-        coinCount = 0;
+        coinCount = 0; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        coinText.text = coinCount.ToString();
     }
 }
